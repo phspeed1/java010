cfa9ce023fb2ffe97219b8133797342f6c883c5e
LG_ParkHonSeark <phspeed1@gmail.com>
2023-05-25 15:08:36 +0900

    first commit

---
+++ D_DP/Fivonachi010.java
@@ -0,0 +1,15 @@
+package D_DP;
+public class Fivonachi010 {
+    public static void main(String args[]) {
+        int in = 7;
+        int out;
+        out = solution(in);
+        System.out.println("out : "+out);
+    }
+
+    static int solution(int n){
+        int out = 0;
+
+        return out;
+    }
+}
---
+++ test010.java
@@ -0,0 +1,7 @@
+
+class test010{
+    public static void main(String args[]){
+        System.out.println("hello 월드");
+        java.util.Arrays.sort(null, 0, 0, null);
+    }
+}
