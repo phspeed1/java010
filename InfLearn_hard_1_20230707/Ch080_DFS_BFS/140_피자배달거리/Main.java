fd0cc64eda6ac0321550c250d317e4fc1fed7654
phspeed1 <phspeed1@gmail.com>
2023-07-11 19:13:50 +0900

    11

--- InfLearn_hard_1_20230707/Ch080_DFS_BFS/140_피자배달거리/Main.java
+++ InfLearn_hard_1_20230707/Ch080_DFS_BFS/140_피자배달거리/Main.java
@@ -19,9 +19,57 @@ public class Main{
 
 	int answer;
 	int solution(int N, int M, int[][] board){
-		answer = 0;
+		answer = Integer.MAX_VALUE;
+
+		List<Point> houseList = new ArrayList<Point>();
+		List<Point> pizzaList = new ArrayList<Point>();
+		
+		for(int i=0; i<N; i++){
+			for(int j=0; j<N; j++){
+				if(board[i][j] == 1){
+					houseList.add(new Point(i, j));
+				}
+				if(board[i][j] == 2){
+					pizzaList.add(new Point(i, j));
+				}
+			}
+		}
+
+		combination(houseList, pizzaList, M, 0, 0, new ArrayList<Point>());
+
 		return answer;
 	}
 
+	void combination(List<Point> houseList, List<Point> pizzaList, int M, int lv, int s, List<Point> resList){
+		if(lv == M){
+			int sum = 0;
+			for(Point hl : houseList){
+				int min = Integer.MAX_VALUE;
+				for(Point pl : resList){
+					min = Math.min(min,   Math.abs(pl.x - hl.x) + Math.abs(pl.y - hl.y)   );
+				}
+				sum += min;
+			}
+	//		System.out.println(sum+" -> "+resList);
+			answer = Math.min(answer, sum);
+			return;
+		}
+		
+		for(int i=s; i<pizzaList.size(); i++){
+			resList.add(pizzaList.get(i));
+			combination(houseList, pizzaList, M, lv+1, i+1, resList);
+			resList.remove(resList.size() -1);
+		}
+	}
 
+	class Point {
+		int x, y;
+		Point(int x, int y){
+			this.x = x;
+			this.y = y;
+		}
+		public String toString(){
+			return "["+x+","+y+"]";
+		}
+	}
 }
